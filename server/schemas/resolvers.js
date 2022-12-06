1b790f54ff8f9a973b59cb20a269ac3d97b74917
Abigail Douglas <EburgAbby@aol.com>
2022-12-05 18:58:35 -0800

    Got background to render

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate('acounts');
+      return User.find().populate('accounts');
     },
     user: async (parent, { username }) => {
       return User.findOne({ username }).populate('accounts');
